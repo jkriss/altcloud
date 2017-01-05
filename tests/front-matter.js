82feffb026d6ab3ae23ac8b78d6d83b44827263d
Jesse Kriss <jkriss@netflix.com>
2017-01-05 09:12:55 -0800

    Starting to add tests

---
+++ tests/front-matter.js
@@ -0,0 +1,22 @@
+const test = require('tape')
+const frontMatter = require('../lib/front-matter')
+const httpMocks = require('node-mocks-http')
+
+test('parse front matter', function (t) {
+  t.plan(3)
+
+  const req = httpMocks.createRequest({
+    method: 'GET',
+    url: '/example.md'
+  })
+
+  const res = httpMocks.createResponse()
+
+  const handler = frontMatter({root: `${__dirname}/data/`})
+
+  handler(req, res, function (err) {
+    t.error(err)
+    t.equal(req.altcloud.attributes.title, 'test file')
+    t.equal(req.altcloud.fileContents, 'markdown content')
+  })
+})
