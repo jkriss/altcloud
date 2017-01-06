55c66fb5f596c9d6763f2f2a4294594a530968e8
Jesse Kriss <jkriss@netflix.com>
2017-01-06 09:14:06 -0800

    Better auth rule enforcement, adding tests

--- tests/access-enforcement.js
+++ tests/access-enforcement.js
@@ -27,9 +27,53 @@ test("don't let unauthed users view a restricted resource", function (t) {
     method: 'GET',
     url: '/user1secrets.txt',
     altcloud: {
-      rules: {
-        read: 'user1'
-      }
+      rules: 'user1'
+    }
+  })
+
+  const res = httpMocks.createResponse()
+
+  const handler = accessEnforcement({root: `${__dirname}/data/`})
+
+  // don't leak info–return 404 if not authorized
+  handler(req, res, function (err) {
+    t.ok(err)
+    t.equals(err.status, 404)
+  })
+})
+
+test("let authed users view a restricted resource if they're on the list", function (t) {
+  t.plan(2)
+
+  const req = httpMocks.createRequest({
+    method: 'GET',
+    url: '/user1secrets.txt',
+    user: 'user1',
+    altcloud: {
+      rules: 'user1'
+    }
+  })
+
+  const res = httpMocks.createResponse()
+
+  const handler = accessEnforcement({root: `${__dirname}/data/`})
+
+  // don't leak info–return 404 if not authorized
+  handler(req, res, function (err) {
+    t.error(err)
+    t.equals(res.statusCode, 200)
+  })
+})
+
+test("don't let authed users view a restricted resource if they're not on the list", function (t) {
+  t.plan(2)
+
+  const req = httpMocks.createRequest({
+    method: 'GET',
+    url: '/user1secrets.txt',
+    user: 'user2',
+    altcloud: {
+      rules: 'user1'
     }
   })
 
