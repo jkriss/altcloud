eb0776db4832a013ba9828c4b121e4757bc53bee
Jesse Kriss <jkriss@netflix.com>
2017-01-15 16:09:07 -0800

    Better handling of special file access enforcement

--- tests/access-enforcement.js
+++ tests/access-enforcement.js
@@ -130,3 +130,113 @@ test("don't let authed users view a restricted resource if they're not on the li
     t.equals(err.status, 404)
   })
 })
+
+test("don't let people view .access if there's no rule for it", function (t) {
+  t.plan(2)
+
+  const req = httpMocks.createRequest({
+    method: 'GET',
+    url: '/.access',
+    user: 'user2'
+  })
+
+  const res = httpMocks.createResponse()
+
+  const handler = accessEnforcement({root: `${__dirname}/data/`})
+
+  // don't leak info–return 404 if not authorized
+  handler(req, res, function (err) {
+    t.ok(err)
+    t.equals(err.status, 404)
+  })
+
+})
+
+test("don't let people view .tokens if there's no rule for it", function (t) {
+  t.plan(2)
+
+  const req = httpMocks.createRequest({
+    method: 'GET',
+    url: '/.tokens',
+    user: 'user2'
+  })
+
+  const res = httpMocks.createResponse()
+
+  const handler = accessEnforcement({root: `${__dirname}/data/`})
+
+  // don't leak info–return 404 if not authorized
+  handler(req, res, function (err) {
+    t.ok(err)
+    t.equals(err.status, 404)
+  })
+
+})
+
+test("don't let people view .keys/private.key if there's no rule for it", function (t) {
+  t.plan(2)
+
+  const req = httpMocks.createRequest({
+    method: 'GET',
+    url: '/.keys/private.key',
+    user: 'user2'
+  })
+
+  const res = httpMocks.createResponse()
+
+  const handler = accessEnforcement({root: `${__dirname}/data/`})
+
+  // don't leak info–return 404 if not authorized
+  handler(req, res, function (err) {
+    t.ok(err)
+    t.equals(err.status, 404)
+  })
+
+})
+
+test("don't let people view .keys/private.key EVER", function (t) {
+  t.plan(2)
+
+  const req = httpMocks.createRequest({
+    method: 'GET',
+    url: '/.keys/private.key',
+    user: 'user2',
+    altcloud: {
+      rules: 'user2'
+    }
+  })
+
+  const res = httpMocks.createResponse()
+
+  const handler = accessEnforcement({root: `${__dirname}/data/`})
+
+  // don't leak info–return 404 if not authorized
+  handler(req, res, function (err) {
+    t.ok(err)
+    t.equals(err.status, 404)
+  })
+
+})
+
+test("let people view .keys/public.pem if it's explicitly allowed", function (t) {
+  t.plan(2)
+
+  const req = httpMocks.createRequest({
+    method: 'GET',
+    url: '/.keys/public.pem',
+    altcloud: {
+      rules: 'all'
+    }
+  })
+
+  const res = httpMocks.createResponse()
+
+  const handler = accessEnforcement({root: `${__dirname}/data/`})
+
+  // don't leak info–return 404 if not authorized
+  handler(req, res, function (err) {
+    t.error(err)
+    t.equals(res.statusCode, 200)
+  })
+
+})
