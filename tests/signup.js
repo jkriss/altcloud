f1a626f1292bf50e81a147ee142a1283302d5a37
Jesse Kriss <jkriss@netflix.com>
2017-01-25 16:56:06 -0800

    Filling in more signup tests

--- tests/signup.js
+++ tests/signup.js
@@ -46,23 +46,178 @@ test('allow new user registration with invitation token', function (t) {
   })
 })
 
-// test("don't allow expired invitation", function (t) {
-// }
+test("don't allow expired invitation", function (t) {
+  t.plan(3)
 
-// test("don't allow bad (unlisted) token", function (t) {
-// }
+  // this one expired one minute ago
+  const invitationCode = `some-old-invitation-code:
+  expires: ${new Date().getTime() - (1000 * 60)}
+  `
 
-// test("don't allow signup without a token", function (t) {
-// }
+  fs.writeFileSync(invitationsPath, invitationCode)
+
+  const req = httpMocks.createRequest({
+    method: 'POST',
+    url: '/signup',
+    body: {
+      token: 'some-old-invitation-code',
+      username: 'newuser',
+      password: 'newpassword'
+    }
+  })
+
+  const res = httpMocks.createResponse()
+
+  const handler = signup({root: `${__dirname}/data/`})
+
+  handler(req, res, function (err) {
+    t.true(err)
+    t.notOk(req.user)
+
+    const invitations = loadInvitations()
+    t.false(invitations['some-old-invitation-code'], 'invitation should no longer be valid')
+  })
+})
+
+test("don't allow bad (unlisted) token", function (t) {
+  t.plan(2)
+
+  const req = httpMocks.createRequest({
+    method: 'POST',
+    url: '/signup',
+    body: {
+      token: 'not-a-code',
+      username: 'newuser',
+      password: 'newpassword'
+    }
+  })
+
+  const res = httpMocks.createResponse()
+
+  const handler = signup({root: `${__dirname}/data/`})
+
+  handler(req, res, function (err) {
+    t.true(err)
+    t.notOk(req.user)
+  })
+})
+
+test("don't allow signup without a token", function (t) {
+  t.plan(2)
+
+  const req = httpMocks.createRequest({
+    method: 'POST',
+    url: '/signup',
+    body: {
+      username: 'newuser',
+      password: 'newpassword'
+    }
+  })
+
+  const res = httpMocks.createResponse()
+
+  const handler = signup({root: `${__dirname}/data/`})
+
+  handler(req, res, function (err) {
+    t.true(err)
+    t.notOk(req.user)
+  })
+})
 
 // test("don't allow bad passwords", function (t) {
-// }
+// })
 
-// test("don't allow missing password", function (t) {
-// }
+test("don't allow missing password", function (t) {
+  t.plan(3)
 
-// test("don't allow missing username", function (t) {
-// }
+  const invitationCode = `some-invitation-code:
+  expires: ${new Date().getTime() + (1000 * 60 * 60 * 24 * 2)}
+  `
 
-// test("don't allow duplicate usernames", function (t) {
-// }
+  fs.writeFileSync(invitationsPath, invitationCode)
+  passwords.remove(`${__dirname}/data/.passwords`, 'newuser')
+
+  const req = httpMocks.createRequest({
+    method: 'POST',
+    url: '/signup',
+    body: {
+      token: 'some-invitation-code',
+      username: 'newuser'
+    }
+  })
+
+  const res = httpMocks.createResponse()
+
+  const handler = signup({root: `${__dirname}/data/`})
+
+  handler(req, res, function (err) {
+    t.ok(err)
+    t.notOk(req.user)
+
+    const invitations = loadInvitations()
+    t.true(invitations['some-invitation-code'], 'invitation should still be valid')
+  })
+})
+
+test("don't allow missing username", function (t) {
+  t.plan(3)
+
+  const invitationCode = `some-invitation-code:
+  expires: ${new Date().getTime() + (1000 * 60 * 60 * 24 * 2)}
+  `
+
+  fs.writeFileSync(invitationsPath, invitationCode)
+
+  const req = httpMocks.createRequest({
+    method: 'POST',
+    url: '/signup',
+    body: {
+      token: 'some-invitation-code',
+      password: 'some-password'
+    }
+  })
+
+  const res = httpMocks.createResponse()
+
+  const handler = signup({root: `${__dirname}/data/`})
+
+  handler(req, res, function (err) {
+    t.ok(err)
+    t.notOk(req.user)
+
+    const invitations = loadInvitations()
+    t.true(invitations['some-invitation-code'], 'invitation should still be valid')
+  })
+})
+
+test("don't allow duplicate usernames", function (t) {
+  t.plan(3)
+
+  const invitationCode = `some-invitation-code:
+  expires: ${new Date().getTime() + (1000 * 60 * 60 * 24 * 2)}
+  `
+
+  fs.writeFileSync(invitationsPath, invitationCode)
+
+  const req = httpMocks.createRequest({
+    method: 'POST',
+    url: '/signup',
+    body: {
+      token: 'some-invitation-code',
+      username: 'user1',
+      password: 'some-password'
+    }
+  })
+
+  const res = httpMocks.createResponse()
+
+  const handler = signup({root: `${__dirname}/data/`})
+
+  handler(req, res, function (err) {
+    t.ok(err)
+    t.notOk(req.user)
+
+    const invitations = loadInvitations()
+    t.true(invitations['some-invitation-code'], 'invitation should still be valid')
+  })
+})
